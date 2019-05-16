aea321151bdf7ee3b4ec3b02777abfe79c8d9ade
Mozart123 <40238324+Mozart123@users.noreply.github.com>
2019-05-16 18:18:50 +0300

    Update proj_model.py

--- proj_model.py
+++ proj_model.py
@@ -1,12 +1,30 @@
 # coding: utf-8
 
-# model code from:
-# https://github.com/soribadiaby/Deep-Learning-liver-segmentation-project/blob/master/train.ipynb
+# Keras model for the UNET architecture. I borrowed the model function from,
+# https://github.com/zhixuhao/unet/blob/master/model.py
+# Due to class imbalance, I used weighted log loss instead of binary crossentropy.
+
+# In[ ]:
 
 from keras.layers import *
 from keras.models import Model, model_from_json
+from keras.layers import Dense, Conv2D, Input, MaxPool2D, UpSampling2D, Concatenate, Conv2DTranspose, BatchNormalization, LeakyReLU
 from keras.optimizers import Adam
 import keras.backend as K
+import keras.losses
+import tensorflow as tf
+
+def weighted_log_loss(y_true, y_pred):
+    # scale predictions so that the class probas of each sample sum to 1
+    y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+    # clip to prevent NaN's and Inf's
+    y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+    # weights are assigned in this order : lesion, liver
+    weights=np.array([40,2,1])
+    weights = K.variable(weights)
+    loss = y_true * K.log(y_pred) * weights
+    loss = K.mean(-K.sum(loss, -1))
+    return loss
 
 def dice_coef(y_true, y_pred, smooth=1):
     intersection = K.sum(y_true * y_pred, axis=[1,2,3])
@@ -16,7 +34,11 @@ def dice_coef(y_true, y_pred, smooth=1):
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
 
-def unet(pretrained_weights = None,input_size = (256,256,1), lr = 1e-4):
+keras.losses.dice_coef_loss = dice_coef_loss
+keras.losses.weighted_log_loss = weighted_log_loss
+keras.metrics.dice_coef_loss = dice_coef_loss
+
+def unet(pretrained_weights = None,input_size = (256,256,1), lr = 1e-4, beta_1=0.9, beta_2=0.999, d_rate = 0.5):
     inputs = Input(input_size)
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
@@ -29,12 +51,12 @@ def unet(pretrained_weights = None,input_size = (256,256,1), lr = 1e-4):
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
     conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
     conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-    drop4 = Dropout(0.5)(conv4)
+    drop4 = Dropout(d_rate)(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
     conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
     conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-    drop5 = Dropout(0.5)(conv5)
+    drop5 = Dropout(d_rate)(conv5)
 
     up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
     merge6 = concatenate([drop4,up6], axis = 3)
@@ -51,30 +73,15 @@ def unet(pretrained_weights = None,input_size = (256,256,1), lr = 1e-4):
     conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
     conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
 
-    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+    up9 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8)) #original is 64
     merge9 = concatenate([conv1,up9], axis = 3)
-    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9) #original is 64
+    conv9 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9) #original is 64
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv9 = Conv2D(6, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9) #Changed filter number to 6
-    conv10 = Conv2D(3, 1, activation = 'sigmoid')(conv9) #Changed filter number to 3
-
-    """
-    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
-    merge9 = concatenate([conv1,up9], axis = 3)
-    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
-    """ 
+    conv10 = Conv2D(3, 1, activation = 'sigmoid')(conv9)
     
     model = Model(inputs = inputs, outputs = conv10)
     
-    model.compile(optimizer = Adam(lr = lr), loss = dice_coef_loss, metrics = ['accuracy'])
-    
-    #model.summary()
-
-    if(pretrained_weights):
-        model.load_weights(pretrained_weights)
+    model.compile(optimizer = Adam(lr = lr, beta_1=beta_1, beta_2=beta_2), loss = weighted_log_loss, metrics = [dice_coef_loss]) #gen_dice_loss
 
     return model
-
